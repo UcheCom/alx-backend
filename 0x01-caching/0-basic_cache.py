e777bf1c641a6623a6ccebbb4ee4ee7707b0c048
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-03 23:36:09 +0100

    Readme file

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""0. Basic dictionary"""
+
+BasicCaching = __import__('basic_caching').BasicCaching
+
+
+class BasicCache(BaseCaching):
+    """A caching system that inherits from the parent class
+       - BaseCaching
+    """
+
+    def put(self, key, item):
+        """Puts or adds a new item to the cache system"""
+        if key and item:
+            self.cache_data[key] = item
+
+
+
+    def get(self, key):
+        """ Return the value linked to key in the cache"""
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
