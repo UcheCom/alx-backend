619adba40a2274506b8331a7341cc3ffe9335f87
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-04 13:18:13 +0100

    FIFO caching system

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,22 +8,19 @@ class FIFOCache(BaseCaching):
     """FIFO caching system that inherits from the parent class
        - BaseCaching
     """
-    def __init__(self):
-        """Initializes the class with the parent's init method"""
-        super().__init__()
-        self.order = []
 
     def put(self, key, item):
         """Puts or adds a new item to the cache system"""
-        if key is None and item is None:
-            pass
-        else:
-            leng = len(self.cache_data)
-            if leng >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[0]))
-                del self.cache_data[self.order[0]]
-                del self.order[0]
-            self.order.append(key)
+        keys = self.cache_data.keys()
+
+        if key and item:
+            if key in keys:
+                self.cache_data[key] = item
+        elif len(keys) >= BaseCaching.MAX_ITEMS:
+            key_ist = list(keys)[0]
+            print("DISCARD: {}".format(key_ist))
+            del self.cache_data[key_ist]
+
             self.cache_data[key] = item
 
     def get(self, key):
