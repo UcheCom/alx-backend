de25f64e7c7110f4c5bf14a3b392d036bb588ccc
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-05 23:20:28 +0100

    FIFO caching system fixed

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
-"""FIFO caching System"""
+"""FIFO caching"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFO caching system that inherits from the parent class
-       - BaseCaching
-    """
+    """FIFO Caching"""
 
     def put(self, key, item):
-        """Puts or adds a new item to the cache system"""
+        """ Add a new item to the cache"""
         keys = self.cache_data.keys()
 
         if key and item:
             if key in keys:
                 self.cache_data[key] = item
-        elif len(keys) >= BaseCaching.MAX_ITEMS:
-            key_ist = list(keys)[0]
-            print("DISCARD: {}".format(key_ist))
-            del self.cache_data[key_ist]
+            elif len(keys) >= BaseCaching.MAX_ITEMS:
+                key_0 = list(keys)[0]
+                print("DISCARD: {}".format(key_0))
+                del self.cache_data[key_0]
 
             self.cache_data[key] = item
 
     def get(self, key):
-        if key is None and key not in self.cache_data.keys():
+        """ Get an item from the cache by key"""
+        if not key and key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
