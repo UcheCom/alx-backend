707d81512accbb98d39f20d28c9a825e764fa9de
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-10 14:49:44 +0100

    Updated

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,6 +2,7 @@
 """Module is Basic Flask and Babel app setup"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask_babel import gettext as _
 
 
 class Config(object):
@@ -15,19 +16,20 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+babel.init_app(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Determines the best match with supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index() -> str:
+def get_index() -> str:
     """Hello world"""
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
