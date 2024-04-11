35f82d89880c56d4ab669a33c6dee2689e3121e6
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-11 23:26:48 +0100

    Error fixed

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -33,7 +33,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel = Babel(app, locale_selector=get_locale)
+# babel = Babel(app, locale_selector=get_locale)
 
 
 def get_user() -> Union[Dict, None]:
